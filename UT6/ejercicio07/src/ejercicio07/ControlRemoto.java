6e2eea017ddfcfe94cadaf0fe4ebc385f89276e0
vjp-enriqueMA <emonteroa06@informatica.iesvalledeljerteplasencia.es>
2025-12-08 12:27:05 +0100

    Tema 6 - Ejercicio 7

---
+++ UT6/ejercicio07/src/ejercicio07/ControlRemoto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ejercicio07;
+
+/**
+ *
+ * @author Enrique
+ */
+public interface ControlRemoto {
+    // MÉTODOS
+    public void apagar();
+    public void encender();
+    public void bajarVolumen();
+    public void subirVolumen();
+    public void cambiarCanal(float canal);
+}
---
+++ UT6/ejercicio07/src/ejercicio07/Test.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio07;
+
+/**
+ *
+ * @author Enrique
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // INSTANCIA TELEVISOR
+        Televisor televisor = new Televisor();
+        televisor.encender();
+        System.out.println(televisor.toString());
+        
+        televisor.subirVolumen();
+        televisor.subirVolumen();
+        televisor.subirVolumen();
+        System.out.println(televisor.getVolumen());
+        
+        televisor.cambiarCanal(10);
+        
+        televisor.bajarVolumen();
+        
+        televisor.apagar();
+        
+        System.out.println(televisor.toString());
+        
+        // INSTANCIA RADIO
+        Radio radio = new Radio();
+        radio.encender();
+        System.out.println(radio.toString());
+        
+        radio.subirVolumen();
+        radio.subirVolumen();
+        radio.subirVolumen();
+        System.out.println(radio.getVolumen());
+        
+        radio.cambiarCanal(20);
+        
+        radio.bajarVolumen();
+        
+        radio.apagar();
+        
+        System.out.println(radio.toString());
+        
+    }
+    
+}
