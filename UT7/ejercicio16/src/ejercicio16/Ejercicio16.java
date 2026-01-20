ce83a502135f6eb22a4f15ea1454bf6bb0ab8c69
vjp-enriqueMA <emonteroa06@informatica.iesvalledeljerteplasencia.es>
2026-01-20 18:03:39 +0100

    Tema 7 - Ejercicio 16

--- UT7/ejercicio16/src/ejercicio16/Ejercicio16.java
+++ UT7/ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -25,48 +25,85 @@ public class Ejercicio16 {
     
     public static int[][] rellenarNotas() {
         Scanner entrada = new Scanner(System.in);
-        int [] [] matriz = new int [6] [4];
-        int i;
-        int j;
-        for (i = 0; i < matriz.length; i++) {
-            System.out.println("Alumno 1 "+(i+1)+"");
-            for (j = 0; j < matriz[0].length; j++) {
-                System.out.println("Nota 1 "+(j+1)+"");
+        int[][] matriz = new int[6][4];
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Alumno " + (i+1));
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.println("Nota " + (j+1));
                 matriz[i][j] = entrada.nextInt();
             }
         }
         return matriz;
     }
     
-    public static void mostrarNotas(int [][] matriz) {
-        int i;
-        int j;
-        for (i = 0; i < matriz.length; i++) {
-            for (j = 0; j < matriz[0].length; j++) {
-                System.out.println(matriz[i][j]);
+    public static void mostrarNotas(int[][] matriz) {
+        String[] nombres = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        String[] asignaturas = {"Lengua", "Mates", "Historia", "Física"};
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.println("Notas de " + nombres[i] + ":");
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.println(asignaturas[j] + ": " + matriz[i][j]);
             }
+            System.out.println();
         }
     }
     
     public static void alumnoMejor(int[][] matriz) {
-        String [] nombres = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
-        int i;
-        int j;
-        double media;
-        for (i = 0; i < matriz.length; i++) {
+        String[] nombres = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        double mejorMedia = -1;
+        int indiceMejor = -1;
+        for (int i = 0; i < matriz.length; i++) {
             int suma = 0;
-            for (j = 0; j < matriz[0].length; j++) {
-                suma = suma + matriz[i][j];
+            for (int j = 0; j < matriz[0].length; j++) {
+                suma += matriz[i][j];
             }
-            media = suma / 4;
+            double media = suma / 4.0;
+            if (media > mejorMedia) {
+                mejorMedia = media;
+                indiceMejor = i;
+            }
         }
+        System.out.println("El alumno con mejor media es " + nombres[indiceMejor] + " con media " + mejorMedia);
     }
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static void alumnoMasSuspensos(int[][] matriz) {
+        String[] nombres = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        int maxSuspensos = -1;
+        int indice = -1;
+        for (int i = 0; i < matriz.length; i++) {
+            int suspensos = 0;
+            for (int j = 0; j < matriz[0].length; j++) {
+                if (matriz[i][j] < 5) suspensos++;
+            }
+            if (suspensos > maxSuspensos) {
+                maxSuspensos = suspensos;
+                indice = i;
+            }
+        }
+        System.out.println("El alumno con más suspensos es " + nombres[indice] + " con " + maxSuspensos + " suspensos");
+    }
+    
+    public static void asignaturaDificil(int[][] matriz) {
+        String[] asignaturas = {"Lengua", "Mates", "Historia", "Física"};
+        double mediaMasBaja = 100;
+        int indice = -1;
+        for (int j = 0; j < matriz[0].length; j++) {
+            int suma = 0;
+            for (int i = 0; i < matriz.length; i++) {
+                suma += matriz[i][j];
+            }
+            double media = suma / 6.0;
+            if (media < mediaMasBaja) {
+                mediaMasBaja = media;
+                indice = j;
+            }
+        }
+        System.out.println("La asignatura más difícil es " + asignaturas[indice] + " con media " + mediaMasBaja);
+    }
+
     public static void main(String[] args) {
         int opcion;
-        int [] [] matriz = new int [6] [4];
+        int[][] matriz = new int[6][4];
         do {
             opcion = insertarMenu();
             switch (opcion) {
@@ -77,9 +114,20 @@ public class Ejercicio16 {
                     mostrarNotas(matriz);
                     break;
                 case 3:
-                    
+                    alumnoMejor(matriz);
+                    break;
+                case 4:
+                    alumnoMasSuspensos(matriz);
+                    break;
+                case 5:
+                    asignaturaDificil(matriz);
+                    break;
+                case 6:
+                    System.out.println("Has salido del programa");
+                    break;
+                default:
+                    System.out.println("Introduce una opción válida");
             }
-        } while (opcion!=6);
+        } while (opcion != 6);
     }
-    
 }
