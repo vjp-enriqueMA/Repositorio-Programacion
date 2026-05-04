bee10656d1dd7b943dbafde2e66c75e4a70f8565
vjp-enriqueMA <emonteroa06@informatica.iesvalledeljerteplasencia.es>
2026-05-04 17:58:29 +0200

    Tema 12 - Ejercicio 26

--- UT12/ejercicio26/src/ejercicio26/Ejercicio26.java
+++ UT12/ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
@@ -29,9 +30,106 @@ public class Ejercicio26 {
         System.out.println("3. Salir del programa");
     }
     
-    public static void introduccionBebidas() throws InputMismatchException {
+    public static void introduccionBebidas() throws InputMismatchException, FileNotFoundException, ClassNotFoundException, IOException {
+        Scanner entrada = new Scanner(System.in);
+        String añadirBebida;
         
+        try(
+                FileOutputStream fos = new FileOutputStream(FICHERO); 
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+            do {
+                System.out.println("Introduce el nombre de la bebida");
+                String nombre = entrada.nextLine();
+
+                System.out.println("Introduce el precio de la bebida");
+                float precio = entrada.nextFloat();
+                entrada.nextLine();
+
+                System.out.println("Introduce el stock de la bebida");
+                int stock = entrada.nextInt();
+                entrada.nextLine();
+                
+                oos.writeObject(new Bebida(nombre, precio, stock));
+                
+                System.out.println("¿Quiéres añadir otra bebida? (SI/NO)");
+                añadirBebida = entrada.nextLine();
+                
+            } while (añadirBebida.equalsIgnoreCase("SI"));
+        }
     }
+    
+    public static void comprarProductos() throws FileNotFoundException, ClassNotFoundException, IOException {
+        Scanner entrada = new Scanner(System.in);
+
+        ArrayList<Bebida> lista = new ArrayList<>();
+
+        float total = 0;
+        String seguirComprando;
+
+        try (
+                FileInputStream fis = new FileInputStream(FICHERO);
+                ObjectInputStream ois = new ObjectInputStream(fis);
+        ) {
+            Bebida bebida;
+
+            while (true) {
+                bebida = (Bebida) ois.readObject();
+                lista.add(bebida);
+            }
+        } catch (EOFException e) {
+            System.out.println("Fin del fichero");
+        }
+
+        do {
+            System.out.println("LISTA PRODUCTOS");
+            
+            for (int i = 0; i < lista.size(); i++) {
+                System.out.println(""+(i+1)+": "+lista.get(i).getNombreBebida()+", "+lista.get(i).getPrecio()+", "+lista.get(i).getStock()+"");
+            }
+
+            System.out.print("Selecciona un producto ");
+            int opcion = entrada.nextInt();
+            entrada.nextLine();
+
+            if (opcion < 1 || opcion > lista.size()) {
+                System.out.println("Producto no válido");
+                
+            } else {
+                Bebida bebidaSeleccionada = lista.get(opcion - 1);
+                
+                System.out.print("¿Cuántas unidades deseas? ");
+                int unidades = entrada.nextInt();
+                entrada.nextLine();
+
+                if (unidades > bebidaSeleccionada.getStock()) {
+                    System.out.println("No hay suficiente stock");
+                    System.out.println("Solo quedan disponibles "+bebidaSeleccionada.getStock()+" unidades.");
+
+                } else {
+                    bebidaSeleccionada.setStock(bebidaSeleccionada.getStock() - unidades);
+                    total = total + (unidades * bebidaSeleccionada.getPrecio());
+                    System.out.println("Producto añadido correctamente");
+                }
+            }
+
+            System.out.print("¿Quiéres comprar otro producto?(SI/NO)");
+            seguirComprando = entrada.nextLine();
+
+        } while (seguirComprando.equalsIgnoreCase("SI"));
+
+        try (
+                FileOutputStream fos = new FileOutputStream(FICHERO);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+        ) {
+            for (int i = 0; i < lista.size(); i++) {
+                oos.writeObject(lista.get(i));
+            }
+        }
+
+        System.out.println("Importe total: "+total+" €");
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -49,7 +147,7 @@ public class Ejercicio26 {
                         introduccionBebidas();
                         break;
                     case 2:
-                        
+                        comprarProductos();
                         break;
                     case 3:
                         System.out.println("Gracias por usar el programa.");
